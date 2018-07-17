b6971f7d1b4c368ff104edb7d1dff26d44611cde
marcedeno <40005945+marcedeno@users.noreply.github.com>
2018-07-17 13:55:15 +0200

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -4,12 +4,30 @@ app.controller('finalProject', function($scope){
    //contenido
 
    var vm = this;
+   vm.accederCuenta = false;
 
+   // Fondo de Pantalla
    vm.img = 'img/wallpaper.jpg';
    vm.background = {'background-image': 'url('+ vm.img +')'};
 
    // Banner
    vm.banner = 'img/banner.jpg';
    vm.backgrount_banner = {'background-image': 'url('+ vm.banner +')'};
+
+   vm.acceder = function(){
+     vm.accederCuenta = true;
+   };
+
+   vm.iniciar = function(){
+     console.log("Hola");
+   };
+
+   vm.registrarme = function(){
+     console.log("Bienvenido");
+   };
+
+   vm.salir = function(){
+     vm.accederCuenta = false;
+   };
 
 });
